edd320a846701f4d2be7a09c9570c63df1005567
Shynobee <w.khouaji@gmail.com>
2018-10-14 21:08:10 +0200

    Creating basic navigation bar

--- src/Main/NavBar/NavBar.jsx
+++ src/Main/NavBar/NavBar.jsx
@@ -1,15 +1,43 @@
-import { AppBar } from './AppBar';
-import { styles } from '@material-ui/core';
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  withStyles,
+  AppBar,
+  Toolbar,
+  Typography
+} from '@material-ui/core';
 
-class NavBar extends Component {
-    render() {
-        return (
-            <div className="root">
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
 
-            </div>
-        );
-    }
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              Sneakers History
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  } 
 }
 
-export default NavBar;
+NavBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+const NavBarWithStyle = withStyles(styles)(NavBar);
+
+export default NavBarWithStyle;
